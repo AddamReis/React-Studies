ddd7e7d3666cba2282865c6bf0569467026c24b4
Addam Reis <addamreis@gmail.com>
2020-10-18 18:16:25 -0300

    Class Component

--- FirstTest/firsttest/src/App.js
+++ FirstTest/firsttest/src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const BemVindo = (props) => {
-    return (
-        <div>
-            <h2>Bem Vindo(a) {props.nome}</h2>
-        </div>
-    );
+class BemVindo extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Bem Vindo(a) {this.props.nome}</h2>
+            </div>
+        );
+    }
 }
 
 function App() {
     return (
         <div>
             <center>
-                <BemVindo nome="Addam"/>
+                <BemVindo nome="Addam" />
                 <br />
+                <hr/>
                 <h1>Iniciando com React em 18/10/2020</h1>
             </center>
         </div>

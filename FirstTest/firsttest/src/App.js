d28d7117699e241a35ac0cfa27365f93632ab7f9
Addam Reis <addamreis@gmail.com>
2020-10-18 18:34:30 -0300

    States

--- FirstTest/firsttest/src/App.js
+++ FirstTest/firsttest/src/App.js
@@ -1,14 +1,45 @@
 import React, { Component } from 'react';
-
 class BemVindo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contador: 0
+        };
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar() {
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state);
+    }
+
+    diminuir() {
+        let state = this.state;
+        if (state.contador === 0) {
+            alert("Opa, Chegou a Zero")
+        } else {
+            state.contador -= 1;
+            this.setState(state);
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2>Bem Vindo(a) {this.props.nome}</h2>
+                <hr />
+                <h3>Contador</h3>
+                <button onClick={this.diminuir}>-</button>
+                    |   {this.state.contador}   |
+                <button onClick={this.aumentar}>+</button>
             </div>
         );
     }
 }
+
+
 
 function App() {
     return (
@@ -16,8 +47,9 @@ function App() {
             <center>
                 <BemVindo nome="Addam" />
                 <br />
-                <hr/>
+                <hr />
                 <h1>Iniciando com React em 18/10/2020</h1>
+                <hr />
             </center>
         </div>
     );

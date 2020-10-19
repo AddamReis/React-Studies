001dda89004dd4ad3204f92105434f7baa609e2a
Addam Reis <addamreis@gmail.com>
2020-10-19 20:02:19 -0300

    Conditional rendering

--- FirstTest/firsttest/src/components/index.js
+++ FirstTest/firsttest/src/components/index.js
@@ -5,23 +5,29 @@ class Membro extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            nome: props.nome
+            nome: props.nome,
+            status: false
         }
         this.entrar = this.entrar.bind(this);
     }
 
     entrar() {
-        this.setState({nome: 'Addam'})
+        this.setState({ nome: 'Addam' })
     }
 
     render() {
         return (
             <div>
                 <center>
-                    <h2>Bem Vindo(a) {this.state.nome}</h2>
+                    {
+                        this.state.status ?
+                            <h2>Bem Vindo(a) {this.state.nome}</h2>
+                            :
+                            <h2>Bem Vindo(a) de volta {this.state.nome}</h2>
+                    }
                     <button onClick={this.entrar}>Entrar</button>
                     <br />
-                    <button onClick={() => this.setState({nome: ''})}>Sair</button>
+                    <button onClick={() => this.setState({ nome: '' })}>Sair</button>
                 </center>
             </div>
         )

06e92a2e0ffdbd389d81f9aebc29153f151ba010
Addam Reis <addamreis@gmail.com>
2020-11-21 14:01:41 -0300

    Firebase config

--- blog/src/App.js
+++ blog/src/App.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Header from './components/Header';
+import firebase from './firebase';
 
 class App extends Component {
+  state ={
+    firebaseInitialized: false
+  };
+
+  componentDidMount(){
+    firebase.isInitialized().then(resultado => {
+      this.setState({firebaseInitialized: resultado})
+    })
+  }
+
   render() {
-    return (
+    return this.state.firebaseInitialized !== false ? (
       <BrowserRouter>
       <Header />
         <Switch>
           <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
+    ) : (
+      <h1>Carregando....</h1>
     )
   }
 }
---
+++ blog/src/firebase.js
@@ -0,0 +1,46 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+let firebaseConfig = {
+    apiKey: "AIzaSyA--C_gd7Z74FIEJ7yryItOAYebqDFJ8w8",
+    authDomain: "blog-eb4bd.firebaseapp.com",
+    databaseURL: "https://blog-eb4bd.firebaseio.com",
+    projectId: "blog-eb4bd",
+    storageBucket: "blog-eb4bd.appspot.com",
+    messagingSenderId: "137249969441",
+    appId: "1:137249969441:web:b75c701fbfe4397964da60",
+    measurementId: "G-L0FEBP1X3C"
+  };
+
+  //if (!firebase.apps.length) {
+    // Initialize Firebase
+  //firebase.initializeApp(firebaseConfig);
+
+class Firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig);
+    }
+
+    login(email, senha){
+        return app.auth().signInWithEmailAndPassword(email, senha)
+    }
+
+    async register(email, senha, nome){
+        await app.auth().createUserWithEmailAndPassword(email, senha)
+
+        const uid = app.auth().currentUser.uid;
+
+        return app.database().ref('Users').child(uid).set({
+            name: nome
+        })
+    }
+
+    isInitialized(){
+        return new Promise(resolve =>{
+            app.auth().onAuthStateChanged(resolve)
+        })
+    }
+}
+
+export default new Firebase();

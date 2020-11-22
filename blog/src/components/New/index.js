5d730c21c6de452454be62b567a3f2a03f0e618e
Addam Reis <addamreis@gmail.com>
2020-11-22 18:19:10 -0300

    Upload Image Part 2 - Final

--- blog/src/components/New/index.js
+++ blog/src/components/New/index.js
@@ -11,7 +11,8 @@ class New extends Component {
             image: null,
             url: '',
             titulo: '',
-            alert: ''
+            alert: '',
+            progress: 0
         }
         this.cadastrar = this.cadastrar.bind(this);
         this.handleFile = this.handleFile.bind(this);
@@ -26,16 +27,16 @@ class New extends Component {
         }
     }
 
-    handleFile = async(e) =>{
+    handleFile = async (e) => {
         e.preventDefault();
-        if(e.target.files[0]){
+        if (e.target.files[0]) {
             const imagem = e.target.files[0];
-            if(imagem.type === 'image/png' || imagem.type === 'image/jpeg'){
-                await this.setState({image: imagem});
+            if (imagem.type === 'image/png' || imagem.type === 'image/jpeg') {
+                await this.setState({ image: imagem });
                 this.handleUpload();
-            }else{
+            } else {
                 alert('Envie uma imagem do tipo PNG ou JPEG');
-                this.setState({image: null});
+                this.setState({ image: null });
                 return null;
             }
         }
@@ -46,13 +47,14 @@ class New extends Component {
 
         if (this.state.titulo !== ''
             && this.state.image !== ''
-            && this.state.descricao !== '') {
+            && this.state.descricao !== ''
+            && this.state.url !== '') {
 
             let posts = firebase.app.ref('Posts');
             let chave = posts.push().key;
             await posts.child(chave).set({
                 titulo: this.state.titulo,
-                image: this.state.image,
+                image: this.state.url,
                 descricao: this.state.descricao,
                 autor: localStorage.nome
             });
@@ -62,25 +64,34 @@ class New extends Component {
         }
     }
 
-    handleUpload = async() =>{
-        const {image} = this.state;
+    handleUpload = async () => {
+        const { image } = this.state;
         const currentUid = firebase.getCurrentId();
 
         const uploadTaks = firebase.storage
-        .ref(`images/${currentUid}/${image.name}`)
-        .put(image);
+            .ref(`images/${currentUid}/${image.name}`)
+            .put(image);
 
-        /*await uploadTaks.on('state_change',
-        (snapshot) =>{
-            //progress
-        },
-        (error) =>{
-            //Error
-            console.log('Erro Imagem: ' + error);
-        },
-        ()=>{
-            //sucesso
-        })*/
+        await uploadTaks.on('state_change',
+            (snapshot) => {
+                //progress
+                const progress = Math.round((
+                    snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+
+                this.setState({ progress });
+            },
+            (error) => {
+                //Error
+                console.log('Erro Imagem: ' + error);
+            },
+            () => {
+                //sucesso
+                firebase.storage.ref(`images/${currentUid}`)
+                    .child(image.name).getDownloadURL()
+                    .then(url => {
+                        this.setState({ url: url });
+                    })
+            })
 
     }
 
@@ -93,17 +104,23 @@ class New extends Component {
 
                 <form onSubmit={this.cadastrar} id="new-post">
                     <span>{this.state.alert}</span>
-                    
-                    <label>Upload Imagem:</label><br />
-                    <input type="file"
-                        onChange={this.handleFile} />
-                    <br />
-                    
-                    <label>Titulo:</label><br />
+
+                    <label>Título:</label><br />
                     <input type="text"
                         placeholder="nome do post" value={this.state.titulo}
                         onChange={(e) => this.setState({ titulo: e.target.value })} />
                     <br />
+
+                    <label>Upload Imagem:</label><br />
+                    <input type="file"
+                        onChange={this.handleFile} />
+                    <br />
+
+                    {this.state.url !== '' ?
+                        <img src={this.state.url} width="250" height="150" alt="Pré visualização"></img>
+                        :
+                        <progress value={this.state.progress} max="100" />}
+                    < br />
 
                     <label>Descrição:</label><br />
                     <textarea type="text"

55fd65c6bb100f2f6a917bd7e374bc2e5a49c61d
Addam Reis <addamreis@gmail.com>
2020-11-23 19:56:53 -0300

    Introduction

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,7 +1,32 @@
+import React, { useState } from 'react';
+
 function App() {
+
+  const [tarefas, setTarefas] = useState([
+    'Aprender C#',
+    'Aprender HTML',
+    'Aprender CSS'
+  ]);
+
+  const [input, setInput] = useState('');
+
+  function handleAdd() {
+    setTarefas([...tarefas, input])
+    setInput('');
+
+    console.log(tarefas);
+  }
+
   return (
     <div>
-      Olá!
+      <ul>
+        {tarefas.map(data => (
+          <li key={data}> {data}</li>
+        ))}
+      </ul>
+
+      <input type="text" value={input} onChange={e => setInput(e.target.value)} />
+      <button type="button" onClick={handleAdd}>Adicionar</button>
     </div>
   );
 }

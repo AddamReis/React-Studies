cf0adcd33d7e37dddd6a36803039ec458db1a820
Addam Reis <addamreis@gmail.com>
2020-11-24 19:34:19 -0300

    Styled Main Page

--- repository/src/pages/Main/index.js
+++ repository/src/pages/Main/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmiteButton } from './style';
 
-export default function Main(){
-    return(
-        <h1>Main</h1>
+export default function Main() {
+    return (
+        <Container>
+            <h1>
+                <FaGithub size={25} />
+                <h1>Meus Repositórios</h1>
+            </h1>
+            <Form onSubmit={() => { }}>
+                <input type="text" placeholder="Adicionar Repositórios" />
+                <SubmiteButton>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmiteButton>
+            </Form>
+        </Container>
     )
 }

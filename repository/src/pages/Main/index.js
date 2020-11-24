fd313d2b807a0bcb10870ef0fb3bdf88dd0b14d5
Addam Reis <addamreis@gmail.com>
2020-11-24 20:13:14 -0300

    Repos search Part 1

--- repository/src/pages/Main/index.js
+++ repository/src/pages/Main/index.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { FaGithub, FaPlus } from 'react-icons/fa';
 import { Container, Form, SubmiteButton } from './style';
+import api from '../../services/api';
 
 export default function Main() {
+    const [newRepo, setNewRepo] = useState('');
+    const [repositorios, setRepositorios] = useState([]);
+
+    const handleSubmit = useCallback((e) => {
+        e.preventDefault();
+
+        async function submit() {
+            const response = await api.get(`repos/${newRepo}`);
+            const data = {
+                name: response.data.full_name,
+            }
+            setRepositorios([...repositorios, data]);
+            setNewRepo('');
+        }
+
+        submit();
+    }, [newRepo, repositorios]);
+
+    function handleInputChance(e) {
+        setNewRepo(e.target.value);
+    }
+
     return (
         <Container>
             <h1>
                 <FaGithub size={25} />
                 <h1>Meus Repositórios</h1>
             </h1>
-            <Form onSubmit={() => { }}>
-                <input type="text" placeholder="Adicionar Repositórios" />
+            <Form onSubmit={handleSubmit}>
+                <input type="text"
+                    placeholder="Adicionar Repositórios"
+                    value={newRepo}
+                    onChange={handleInputChance} />
                 <SubmiteButton>
                     <FaPlus color="#FFF" size={14} />
                 </SubmiteButton>

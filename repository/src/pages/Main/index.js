2b05811075e581eee0451ea4c34eba38b0066c54
Addam Reis <addamreis@gmail.com>
2020-11-26 19:53:43 -0300

    Parameters by URL

--- repository/src/pages/Main/index.js
+++ repository/src/pages/Main/index.js
@@ -2,6 +2,7 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { FaGithub, FaPlus, FaSpinner, FaBars, FaTrash } from 'react-icons/fa';
 import { Container, Form, SubmitButton, List, DeleteButton } from './style';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
 
 export default function Main() {
     const [newRepo, setNewRepo] = useState('');
@@ -92,9 +93,9 @@ export default function Main() {
                                 <FaTrash size={14} />
                             </DeleteButton>
                             {repo.name}</span>
-                        <a href="">
+                        <Link to={`/repositorio/${encodeURIComponent(repo.name)}`}>
                             <FaBars size={20} />
-                        </a>
+                        </Link>
                     </li>
                 ))}
             </List>

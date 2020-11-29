760023b342b30c570276e81f42a2aab8f952711f
Addam Reis <addamreis@gmail.com>
2020-11-29 11:16:42 -0300

    Reducer Config

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Routes from './routes';
 import Header from './components/Header';
+import { Provider } from 'react-redux';
+import store from './store';
 
-export default function App(){
-  return(
-    <BrowserRouter>
-    <Header />
-      <Routes />
-    </BrowserRouter>
+export default function App() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Header />
+        <Routes />
+      </BrowserRouter>
+    </Provider>
   );
 }

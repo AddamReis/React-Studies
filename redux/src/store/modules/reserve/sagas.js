77cd94647c40f51d1589f586cf6975d0cf2c160f
Addam Reis <addamreis@gmail.com>
2020-11-29 14:52:44 -0300

    Refactoring Saga

--- redux/src/store/modules/reserve/sagas.js
+++ redux/src/store/modules/reserve/sagas.js
@@ -1,11 +1,26 @@
-import {call, put, all, takeLatest} from 'redux-saga/effects';
-import {addReserveSucess} from './actions';
+import { select, call, put, all, takeLatest } from 'redux-saga/effects';
+import { addReserveSucess, updateAmount } from './actions';
 import api from '../../../server/api';
 
-function* addToReserve({ id }){
-    const response = yield call(api.get, `trips/${id}`);
+function* addToReserve({ id }) {
 
-    yield put(addReserveSucess(response.data))
+    const tripExists = yield select(
+        state => state.reserve.find(trip => trip.id === id)
+    );
+
+    if (tripExists) {
+        const amount = tripExists.amount +1;
+        yield put(updateAmount(id, amount));
+    } else {
+        const response = yield call(api.get, `trips/${id}`);
+        
+        const data = {
+            ...response.data,
+            amount: 1
+        };
+        
+        yield put(addReserveSucess(data))
+    }
 }
 
 export default all([

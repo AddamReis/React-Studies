a2fc6ae517b33c38763808f345afb448e5328a7f
Addam Reis <addamreis@gmail.com>
2020-11-29 11:01:23 -0300

    Stylized Pages

--- redux/src/pages/Home/index.js
+++ redux/src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../server/api';
+import { MdFlightTakeoff } from 'react-icons/md';
+import './style.css';
 
 export default function Home() {
     const [trips, setTrips] = useState([]);
@@ -15,8 +17,25 @@ export default function Home() {
     }, []);
 
     return (
-        <div>
-            <h1>Home</h1>
+        <div className="box">
+            {trips.map(trip => (
+                <li key={trip.id}>
+                    <img src={trip.image} alt={trip.title} />
+                    <strong>{trip.title}</strong>
+                    <span>Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+
+                    <button
+                        type="button"
+                        onClick={() => { }}
+                    >
+                        <div>
+                            <MdFlightTakeoff size={16} color="#FFF" />
+                        </div>
+                        <span>SOLICITAR RESERVA</span>
+                    </button>
+                </li>
+            ))}
+
         </div>
     );
 }
--- redux/src/pages/Reservas/index.js
+++ redux/src/pages/Reservas/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md'
+import './style.css';
 
-export default function Reservas(){
-    return(
+export default function Reservas() {
+    return (
         <div>
-            Reservas
+            <h1 className="title">Você solicitou 1 reserva</h1>
+
+            <div className="reservas">
+                <img
+                    src=""
+                    alt="Maceio"
+                />
+                <strong>Viagem Maceio 7 dias</strong>
+                <span>Quantidade: 2</span>
+                <button
+                type="button"
+                onClick={() => {}}>
+                    <MdDelete size={20} color="#191919" />
+                </button>
+            </div>
+            <footer>
+                <button type="button">Solicitar Reservas</button>
+            </footer>
         </div>
     );
 }
